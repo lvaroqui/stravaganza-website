91b4df8e72d0c78e10b9aa2339b6180ce75af32e
Luc Varoqui <luc@varoqui.org>
2020-04-05 17:30:14 +0200

    Fix lint-staged in case of success
    tmp
    
    Add stashing before linting staged

--- scripts/lint-staged.sh
+++ scripts/lint-staged.sh
@@ -3,27 +3,16 @@ echo "Running lint-staged"
 FILES=$(git diff --name-only --cached | grep -E "^front/.*\.(js|ts|vue)$" | cut -c 7-)
 
 if [[ -z $FILES ]]; then
-  echo "No files need linting"
-  exit 0 # No file staged to lint
+  echo "No files to lint"
+  exit 0
 fi
 
-# Stashing unstaged changes
-git commit -q --no-verify -m 'Save index'
-git stash -q
-git reset -q --soft HEAD~
+git stash --keep-index
 
 # Running Lint on staged files
 docker-compose run front yarn lint $FILES
 ret=$?
 
-# Re-applying unstaged changes
-if [[ ret -ne 0 ]]; then
-  echo "Linting error detected"
-  git commit -q --no-verify -m 'Save index'
-  git stash pop -q
-  git reset -q HEAD
-  git reset -q --soft HEAD~
-fi
-
+git stash pop
 
 exit $ret
